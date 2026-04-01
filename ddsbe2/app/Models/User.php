05877ec984c4dca4c361019e95cafe75b6eb276c
TheNoob-Grammar <ambag.arwin@gmail.com>
2026-04-01 18:04:51 +0800

    With Security Layer

--- ddsbe2/app/Models/User.php
+++ ddsbe2/app/Models/User.php
@@ -1,14 +1,16 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
     protected $table = 'users2';
+    
 // column sa table
     protected $fillable = 
-        ['username', 'password', 'gender'];
+        ['username', 'password', 'gender','jobid'];
 
     protected $hidden =
         ['password',];
